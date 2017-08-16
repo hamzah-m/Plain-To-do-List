3d21aa1412ccb40a663aa8120a84425aedb246e5
Hamzah Mugharbil <Hamzah@hamzahs-mbp.lan>
2017-08-16 18:02:43 +0300

    Initial finished version of app.

--- Plain To-do List/ViewController.swift
+++ Plain To-do List/ViewController.swift
@@ -8,8 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var textOutput: UITextView!
+    
+    var count = 1
+    var items: [String] = []
+    
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+    
+        if textInput.text! == "" {
+            return
+        }
+        
+        items.append(textInput.text!)
+        textOutput.text = ""
+        for item in items {
+            textOutput.text.append("\(count). \(item)\n")
+        }
+        count += 1
+        textInput.text = ""
+        textInput.resignFirstResponder()
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textInput.resignFirstResponder()
+        textOutput.resignFirstResponder()
+        return false
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textInput.resignFirstResponder()
+        textOutput.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
